d8bed559bf3a5a5d41c74b7c0e98f8628ea7eb38
Tim Platzke <tim.platzke@active-value.de>
2017-10-24 14:11:36 +0200

    added main window handler

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 'use strict';
 
 const winston = require('winston')
-const {app} = require('electron')
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const _ = require('underscore')
+
+const APP_DEBUG = true
 
 const loggerConfig = {
     transports: [
         new (winston.transports.File)({
-            filename: app.getAppPath()+'main.log',
+            filename: path.join(app.getAppPath(),'main.log'),
             maxsize: 2048,
             json: false,
             maxFiles: 5,
@@ -18,5 +22,41 @@ const loggerConfig = {
 }
 const logger = winston.Logger(loggerConfig)
 
+let windows = {};
+const windowOption = {
+    width: 1024,
+    height: 768
+}
 
+function start() {
+    windows.mainWindow = new BrowserWindow(windowOption)
+
+    if(APP_DEBUG)
+    {
+        windows.mainWindow.openDevTools()
+    }
+
+    windows.mainWindow.on('close', () => {
+        delete windows.mainWindow
+    })
+}
+
+
+app.on('ready', start)
+
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (_.isEmpty(windows)) {
+        start();
+    }
+})
 
