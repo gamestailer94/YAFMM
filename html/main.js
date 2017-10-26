5ed3d1d6b0a796209e2c1464125725457d73e9c5
Tim Platzke <tim.platzke@active-value.de>
2017-10-26 18:28:52 +0200

    first react tests

--- html/main.js
+++ html/main.js
@@ -3,3 +3,10 @@
 const $ = require('jquery')
 const Popper = require('popper.js')
 require('./res/js/bootstrap.js')
+
+const React = require('react')
+const ReactDOM = require('react-dom')
+const fs = require('fs')
+fs.readFile('./tpl/main.jxs',function (error,data) {
+    ReactDOM.render(data,document.getElementsByName('body'))
+})
---
+++ html/tpl/main.jsx
@@ -0,0 +1,1 @@
+<div class="w-100 h-75 bg-success">hallo</div>
